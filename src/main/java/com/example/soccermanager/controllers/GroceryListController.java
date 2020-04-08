088acfa463afe6cf263a32bea9ca84ab1895c980
Jindrich Jehlicka <jindrichjehlicka@gmail.com>
2020-04-08 16:25:28 +0200

    add getoneitem

--- src/main/java/com/example/soccermanager/controllers/GroceryListController.java
+++ src/main/java/com/example/soccermanager/controllers/GroceryListController.java
@@ -7,31 +7,38 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class GroceryListController {
+  //  TODO: change
+  public static final String DefaultUrl = "http://localhost:3000";
 
   @Autowired
   GroceryListRepository groceryListRepository;
 
-  @CrossOrigin(origins = "http://localhost:3000")
+  @CrossOrigin(origins = DefaultUrl)
   @GetMapping(path = "/items")
-  public List<Item> getAllItems(){
-    List<Item> items = new ArrayList<>();
-    groceryListRepository.findAll().forEach(items :: add);
-    return items;
+  public List<Item> getAllItems() {
+    return new ArrayList<>(groceryListRepository.findAll());
   }
 
-  @CrossOrigin(origins = "http://localhost:3000")
+  @CrossOrigin(origins = DefaultUrl)
   @PostMapping(path = "/items")
-  public Item addItem(@RequestBody Item item){
+  public Item addItem(@RequestBody Item item) {
     groceryListRepository.save(item);
     return item;
   }
 
-  @CrossOrigin(origins = "http://localhost:3000")
+  @CrossOrigin(origins = DefaultUrl)
   @DeleteMapping(path = "/items/{id}")
-  public void deleteItem(@PathVariable int id){
+  public void deleteItem(@PathVariable int id) {
     groceryListRepository.deleteById(id);
+  }
+
+  @CrossOrigin(origins = DefaultUrl)
+  @GetMapping(path = "/items/{id}")
+  public Optional<Item> getOneItem(@PathVariable int id) {
+    return groceryListRepository.findById(id);
   }
 }
--- src/main/java/com/example/soccermanager/entities/Item.java
+++ src/main/java/com/example/soccermanager/entities/Item.java
@@ -1,4 +1,5 @@
 package com.example.soccermanager.entities;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
